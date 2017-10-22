aaa6408fb68da4ebbf64b85bf2456bf28842cbdf
jiean yang <yja8534786@gmail.com>
2017-10-22 19:30:08 +0200

    Assignment 1

--- node-express/routes/dishRouter.js
+++ node-express/routes/dishRouter.js
@@ -5,10 +5,14 @@ const dishRouter = express.Router();
 
 dishRouter.use(bodyParser.json());
 
+
+module.exports = dishRouter;
+
+
 //	url: /dishes
 dishRouter.route('/')
 //	middleware
-.all((req, res, next) => {
+.all( (req, res, next) => {
 	res.statusCode = 200;
 	res.setHeader('Content-Type','text/plain');
 	next();
@@ -28,4 +32,27 @@ dishRouter.route('/')
 	res.end('Deleting all the dishes!');
 } );
 
-module.exports = dishRouter;
+
+
+//	url: /dishes/:dishId
+dishRouter.route('/:dishId')
+.all( (req, res, next) => {
+	res.statusCode = 200;
+	res.setHeader('Content-Type','text/plain');
+	next();
+} )
+.get( (req, res, next) => {
+	//retrieve the data of id
+	res.end('Will send details of the dish: ' + req.params.dishId + ' to you!');
+} )
+.post( (req, res, next) => {
+	res.statusCode = 403;
+	res.end('POST operation not supported on /dishes/' + req.params.dishId);
+} )
+.put( (req, res, next) => {
+	res.write('Updating the dish: ' + req.params.dishId + '\n');
+	res.end('Will update the dish: ' + req.body.name + ' with details: ' + req.body.description);
+} )
+.delete( (req, res, next) => {
+	res.end('Deleting dish: ' + req.params.dishId);
+} );
