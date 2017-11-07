e781683db00b45bdc745910e16627ac10bca4139
jiean yang <yja8534786@gmail.com>
2017-11-07 23:49:31 +0100

    Passport Facebook

--- conFusionServer/config.js
+++ conFusionServer/config.js
@@ -1,4 +1,8 @@
 module.exports = {
 	'secretKey': '12345-67890-09876-54321',
-	'mongoUrl': 'mongodb://localhost:27017/conFusion'
+	'mongoUrl': 'mongodb://localhost:27017/conFusion',
+	'facebook': {
+		clientId: '196878594191626',
+		clientSecret: '890ca7487549756e7f8187dc312b2c5e'
+	}
 }
